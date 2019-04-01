85b144a7e2c446393a9d91e30b442eb80dba5846
BreadBug007 <47244587+BreadBug007@users.noreply.github.com>
2019-04-01 12:53:51 +0000

    Add files via upload

--- Prob_67.py
+++ Prob_67.py
@@ -1,22 +1,20 @@
 import time
 
-start  = time.time()
+start = time.time()
 
-num = [i.strip().split() for i in open("E:\Sharing\Data\p067_triangle.txt").readlines()]
-
-
+num = [i.strip().split() for i in open("Data\p067_triangle.txt").readlines()]
 
 for i in range(len(num)):
-    num[i] = [int(j) for j in num[i]]  
+    num[i] = [int(j) for j in num[i]]
 
-def main():    
-    for i in range(len(num)-2, -1, -1):
+
+def main():
+    for i in range(len(num) - 2, -1, -1):
         for j in range(len(num[i])):
-            num[i][j] = num[i][j] + max(num[i+1][j], num[i+1][j+1])
-    return num[0][0]
+            num[i][j] = num[i][j] + max(num[i + 1][j], num[i + 1][j + 1])
+    print(num[0][0])
 
-print(main())
 
+main()
 end = time.time()
-
-print(end-start)
+print(end - start)
